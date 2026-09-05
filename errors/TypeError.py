70617ce745af0e95203cb92326ccb4a130d5d082
viveks5898 <39966959+viveks5898@users.noreply.github.com>
2026-09-05 19:10:57 +0530

    typeError during concatenation

---
+++ errors/TypeError.py
@@ -0,0 +1,6 @@
+user_name = "vivek"
+
+age = "34"
+
+print(user_name + age)
+# TypeError: can only concatenate str (not "int") to str
---
+++ variables.py
@@ -0,0 +1,7 @@
+age = 20
+print(age)   # 20
+
+age = 21
+print(age)   # 21
+
+user_name = "vivek singh"
